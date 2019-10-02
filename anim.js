f4d83c3b12f4413b2f28871a062108cc1691afb9
Danish0311 <Danish0311@github.com>
2019-10-02 09:12:47 -0400

    dsfadsf

---
+++ anim.js
@@ -0,0 +1,45 @@
+function ask(){
+	var x =prompt("What's your name?");
+		if (x === 'Danish' || x === 'danish'){
+			alert(x + " " + "is a good name")
+		} else {
+			alert(x + " " + "is an okay name")
+		}
+	}
+
+
+function pop(){
+	alert("heyOOOOOOO");
+}
+
+
+function ask(){
+	var x = prompt("Guess the number between 1 & 100:");
+		while (x != 69) {
+		if (x <= 68) {
+			x = prompt("higher")
+		} else if (x >= 70) {
+			x = prompt("lower")
+		} else {
+			x = prompt("error")
+		}
+	}
+		if (x = 69) {
+			alert("Nice")
+		}
+}
+
+function e(){
+	var x = prompt("Please enter a number");
+	var y = prompt("Enter another one");
+		if (x > y){
+			alert(x)
+		}
+		else if (x < y){
+			alert(y)
+		}
+		else {
+			prompt("That is not a number")
+		}
+}
+
